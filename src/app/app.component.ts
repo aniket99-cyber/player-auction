f208724a036354b397af9457484fd71fddb07865
aniket99-cyber <aniket99bose@gmail.com>
2024-07-28 16:20:51 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { DataService } from './data.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 import { WheelComponent } from './wheel/wheel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WheelComponent,CommonModule, RouterLink ],
+  imports: [RouterOutlet, WheelComponent,CommonModule, RouterLink, SidebarComponent,NavbarComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
 import { TeamsComponent } from './teams/teams.component';
 import { WheelViewComponent } from './wheel-view/wheel-view.component';
 
 export const routes: Routes = [
-  {path:'', component: WheelViewComponent},
+  {path:'', component: OverviewComponent},
+  {path:'auction', component: WheelViewComponent},
   {path:'teams', component:TeamsComponent}
 ];
